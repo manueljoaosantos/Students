8db08d2a074bfaa88bf989504cab142f435e3459
Manuel Santos <manueljoaosantos@gmail.com>
2023-01-29 18:31:31 +0000

    fix: ignore items

---
+++ Quick.Students.Domain/Entities/Guardian.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quick.Students.Domain.Entities
+{
+    public class Guardian:BaseEntity<int>
+    {
+        [Key]
+        public override int Id { get; set; }
+
+        public Family Family { get; set; } = new Family();
+        public int FamilyId { get; set; }
+
+        public Address Address { get; set; } = new Address();
+
+        public GuardianType GuardianType { get; set; } = new GuardianType();
+        public int GuardianTypeId { get; set; }
+        [Required]
+        [StringLength(255,MinimumLength =3)]
+        public string FName { get; set; } = string.Empty;
+        [Required]
+        [StringLength(255, MinimumLength = 3)]
+        public string LName { get; set; } = string.Empty;
+        [Required]
+        [StringLength(254,MinimumLength =3)]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        [MaxLength(15)]
+        public string PhoneNumber { get; set; } = string.Empty;
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # Students
 CRUD com .NET 7, MySQL, Angular 14, Keycloak, Phometheus e Grafana
+
+# Comandos
+dotnet new sln --name Quick.Students
+
+dotnet new classlib -o Quick.Students.Infrastructure
+dotnet sln add Quick.Students.Infrastructure
+code .
+
+cd Quick.Students.Infrastructure
+dotnet restore
+dotnet build
+
+dotnet add package Microsoft.EntityFrameworkCore
+dotnet add package Microsoft.entityframeworkcore.design
+dotnet add package Microsoft.EntityFrameworkCore.Sqlite
+dotnet add package Microsoft.EntityFrameworkCore.Tools
+
+dotnet restore
+dotnet build
+
+dotnet new classlib -o Quick.Students.Domain
+dotnet sln add Quick.Students.Domain
+
+cd .\Quick.Students.Infrastructure\
+dotnet add reference ..\Quick.Students.Domain\
